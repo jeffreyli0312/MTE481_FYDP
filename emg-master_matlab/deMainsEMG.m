551c22d920d8483d5cd82f03a7a53180b302a46f
JD <jiangdizhu@gmail.com>
2026-02-12 18:12:16 -0500

    MATLAB

---
+++ emg-master_matlab/deMainsEMG.m
@@ -0,0 +1,28 @@
+function [filtered] = deMainsEMG(unfiltered)
+%deMainsEMG: removes 60 Hz mains hum from an emg signal
+%   usage:  [filtered] = deMainsEMG(unfiltered);
+%   input:  EMG struct to be filtered
+%   output: notch-filtered EMG struct
+
+% unpack everything
+fs      = unfiltered.fs;
+signal  = unfiltered.signal;
+
+% normalized frequencies
+fn = fs / 2;    % nyquist frequency
+wc = 60 / fn;   % center frequency 60 Hz 
+bw = 1 / fn;    % -3 dB bandwidth 1 Hz
+
+% calculate FIR coefficients
+[b,a] = iirnotch(wc, bw);
+
+% filter signal
+filtsig = filter(b,a,signal);
+
+% transfer
+filtered = unfiltered;
+
+% update signal
+filtered.signal = filtsig;
+
+end
---
+++ emg-master_matlab/importemg.m
@@ -0,0 +1,30 @@
+function [delt,tri] = importemg(filename, fs)
+%importemg: imports emg data from a csv file
+%   usage:  [delt, tri] = importemg(filename, fs);
+%   input:  the filename of the csv you wish to import
+%           columns: deltoid, tricep, start events, stop events
+%           the sampling frequency fs
+%   output: two EMG structs, one for each muscle
+
+M = csvread(filename,1,0); % import whole csv
+
+% extract columns
+tri   = M(:,1); % deltoid data (first column)
+delt  = M(:,2); % tricep data (second column)
+start = M(:,3); % start data (third column)
+stop  = M(:,4); % stop data (fourth column)
+
+% extract start and stop breakpoints
+starts = find(start);
+stops  = find(stop);
+n = min(length(starts),length(stops));
+
+% generate time basis and length
+t = timebasis(delt, fs);
+l = length(delt);
+
+% pack it all into a struct
+delt = struct('signal',delt,'time',t,'l',l,'starts',starts,'stops',stops,'n',n,'fs',fs);
+tri = struct('signal',tri,'time',t,'l',l,'starts',starts,'stops',stops,'n',n,'fs',fs);
+
+end
